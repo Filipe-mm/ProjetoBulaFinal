b098c4553513e0f4095418ff4989048b8d2c9cc4
Filipe Magalhães Moreira <filipe.mm.1999@hotmail.com>
2023-06-20 17:08:32 -0300

    Criação das Interfaces
    criação das interfaces Iservico e IservicoAdm

---
+++ Repositorio/Interfaces/IServico.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoBulaFinal.Repositorio.Interfaces
+{
+    public interface IServico<T>
+    {
+        Task<List<T>> TodosAsync();
+        Task IncluirAsync(T obj);
+        Task<T> AtualizarAsync(T obj);
+        Task ApagarAsync(T obj);
+    }
+}
---
+++ Repositorio/Interfaces/IServicoAdm.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjetoBulaFinal.Repositorio.Interfaces
+{
+    public interface IServicoAdm<T> : IServico<T>
+    {
+        Task<T?> Login(string email, string senha);
+    }
+}
