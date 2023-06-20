69b23be5f4d2b8a8313aaaf09d1415739a86eac0
Filipe Magalhães Moreira <filipe.mm.1999@hotmail.com>
2023-06-20 16:54:20 -0300

    Criação dos modelos 2
    Criação das classes medicamento e administrador

--- Models/Administrador.cs
+++ Models/Administrador.cs
@@ -1,6 +1,37 @@
-﻿namespace ProjetoBulaFinal.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoBulaFinal.Models
 {
     public class Administrador
     {
+        [Column("Id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } = default!;
+
+        [Column("Nome")]
+        public string Nome { get; set; } = default!;
+
+        [Column("Email")]
+        public string Email { get; set; } = default!;
+
+        [Column("Senha")]
+        public string Senha { get; set; } = default!;
+
+        [Column("Permissao")]
+        public string Permissao { get; set; } = default!;
+
+        [Column("CPF")]
+        public string CPF { get; set; } = default!;
+
+        [Column("CRF")]
+        public string CRF { get; set; } = default!;
+
+        [Column("CNAE")]
+        public string CNAE { get; set; } = default!;
+
+        [Column("VISA")]
+        public string VISA { get; set; } = default!;
     }
 }
--- Models/Medicamento.cs
+++ Models/Medicamento.cs
@@ -1,6 +1,43 @@
-﻿namespace ProjetoBulaFinal.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoBulaFinal.Models
 {
     public class Medicamento
     {
+        [Column("Id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } = default!;
+
+        [Column("Marca")]
+        public string Marca { get; set; } = default!;
+
+        [Column("Nome")]
+        public string Nome { get; set; } = default!;
+
+        [Column("Tipo")]
+        public string Tipo { get; set; } = default!;
+
+        [Column("RMS")]
+        public string RMS { get; set; } = default!;
+
+        [Column("EAN")]
+        public string EAN { get; set; } = default!;
+
+        [Column("Ativo")]
+        public string Ativo { get; set; } = default!;
+
+        [Column("Forma")]
+        public string Forma { get; set; } = default!;
+
+        [Column("Via")]
+        public string Via { get; set; } = default!;
+
+        [Column("Unidade")]
+        public string Unidade { get; set; } = default!;
+
+        [Column("Consumidor")]
+        public string Consumidor { get; set; } = default!;
     }
 }
