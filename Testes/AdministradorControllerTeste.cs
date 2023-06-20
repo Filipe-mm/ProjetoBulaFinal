43345635682ba30259ec78d2199d24bdda28771e
Filipe Magalhães Moreira <filipe.mm.1999@hotmail.com>
2023-06-20 18:04:24 -0300

    Teste TDD ReturnsOk (Login)
    Dando sequência para próxima etapa escrevemos um teste para a terceira funcionalidade de login, na qual queríamos implementar, anteriormente fizemos um teste que falhou, pois ainda não tinhamos a implementação (step red). Após essa falha implementarmos essa funcionalidade de login que retornaria um "Resultado Ok com o Administrador Logado" caso o campo de email ou senha for válido e faria o login. Executamos o teste novamente, e dessa vez o teste passou com sucesso. Passamos para o processo de refatoração do código, buscando pontos para melhorias e aplicando boas práticas.

--- Testes/AdministradorControllerTeste.cs
+++ Testes/AdministradorControllerTeste.cs
@@ -7,9 +7,11 @@ using ProjetoBulaFinal.Repositorio.Interfaces;
 
 namespace ProjetoBulaFinal.Testes
 {
+
     [TestFixture]
     public class AdministradorControllerTeste
     {
+
         private AdministradorController _controller;
         private Mock<IServicoAdm<Administrador>> _servicoMock;
 
@@ -49,6 +51,24 @@ namespace ProjetoBulaFinal.Testes
             Assert.That(result, Is.TypeOf<StatusCodeResult>());
             var statusCodeResult = (StatusCodeResult)result;
             Assert.That(statusCodeResult.StatusCode, Is.EqualTo(404));
+        }
+
+        [Test]
+        public async Task Login_WithValidEmailAndSenha_ReturnsOkResultWithAdministradorLogado()
+        {
+            // Arrange
+            var administradorDTO = new AdministradorDTO { Email = "admin@example.com", Senha = "password" };
+            var administrador = new Administrador { Email = "admin@example.com", Senha = "password" };
+            _servicoMock.Setup(s => s.Login(administradorDTO.Email, administradorDTO.Senha)).ReturnsAsync(administrador);
+
+            // Act
+            var result = await _controller.Login(administradorDTO);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<StatusCodeResult>());
+            var statusCodeResult = (StatusCodeResult)result;
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(200));
+            _servicoMock.Verify(s => s.Login(administradorDTO.Email, administradorDTO.Senha), Times.Once);
         }
     }
 }
