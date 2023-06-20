90e3908e4a992465d438d27b6f5376513459a3d0
Filipe Magalhães Moreira <filipe.mm.1999@hotmail.com>
2023-06-20 18:15:52 -0300

    Teste TDD GET (IndexReturnsOkResult) - Medicamentos
    Partindo para o campo de medicamentos, escrevemos um teste para a quarta funcionalidade do projeto e a primeira de medicamentos. Que consiste na busca de todos os medicamentos. Anteriormente fizemos um teste que falhou, pois ainda não tínhamos a implementação (step red). Após essa falha implementamos essa funcionalidade de busca que retornaria um "Resultado Ok" caso encontrasse o medicamento em questão. Executamos o teste novamente, e dessa vez o teste passou com sucesso. Passamos para o processo de refatoração do código, buscando pontos para melhorias e aplicando boas práticas.

---
+++ Testes/MedicamentoControllerTeste.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using ProjetoBulaFinal.Models;
+using ProjetoBulaFinal.Repositorio.Interfaces;
+
+namespace ProjetoBulaFinal.Testes
+{
+    [TestFixture]
+    public class MedicamentoControllerTeste
+    {
+        private MedicamentoController _controller;
+        private Mock<IServico<Medicamento>> _servicoMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _servicoMock = new Mock<IServico<Medicamento>>();
+            _controller = new MedicamentoController(_servicoMock.Object);
+        }
+
+        [Test]
+        public async Task Index_ReturnsOkResultWithMedicamentos()
+        {
+            // Arrange
+            var medicamentos = new List<Medicamento>
+            {
+                new Medicamento { Id = 1, Nome = "Medicamento 1" },
+                new Medicamento { Id = 2, Nome = "Medicamento 2" }
+            };
+            _servicoMock.Setup(s => s.TodosAsync()).ReturnsAsync(medicamentos);
+
+            // Act
+            var result = await _controller.Index();
+
+            // Assert
+            Assert.That(result, Is.TypeOf<OkObjectResult>());
+            var okResult = (OkObjectResult)result;
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+            Assert.That(okResult.Value, Is.EqualTo(medicamentos));
+        }
+    }
+}
