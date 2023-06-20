702bb30ef1aa110cf26e522716af869c16df2041
Filipe Magalhães Moreira <filipe.mm.1999@hotmail.com>
2023-06-20 17:56:25 -0300

    Teste TDD BadRequest (Login)
    Nesta primeira etapa escrevemos um teste para a primeira funcionalidade de login, na qual queriamos implementar, anteriormente fizemos um teste que falhou, pois ainda não tinhamos a implementação (step red). Após essa falha implementarmos essa funcionalidade de login que retornaria um "Bad Request" caso o campo de email ou senha estiver vazio. Executamos o teste novamente, e dessa vez o teste passou com sucesso. Passamos pelo processo de refatoração do código, buscando pontos para melhorias e aplicando boas práticas. Basicamente seguimos essa etapa em todos os outros testes.

---
+++ Testes/AdministradorControllerTeste.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using ProjetoBulaFinal.DTOs;
+using ProjetoBulaFinal.Models;
+using ProjetoBulaFinal.Repositorio.Interfaces;
+
+namespace ProjetoBulaFinal.Testes
+{
+    [TestFixture]
+    public class AdministradorControllerTeste
+    {
+        private AdministradorController _controller;
+        private Mock<IServicoAdm<Administrador>> _servicoMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _servicoMock = new Mock<IServicoAdm<Administrador>>();
+            _controller = new AdministradorController(_servicoMock.Object);
+        }
+
+        [Test]
+        public async Task Login_WithEmptyEmailOrSenha_ReturnsBadRequestResult()
+        {
+            // Arrange
+            var administradorDTO = new AdministradorDTO { Email = "", Senha = "" };
+
+            // Act
+            var result = await _controller.Login(administradorDTO);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<StatusCodeResult>());
+            var statusCodeResult = (StatusCodeResult)result;
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(400));
+        }
+    }
+}
