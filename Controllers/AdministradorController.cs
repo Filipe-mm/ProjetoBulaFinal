0949e00165ba720c5e01313c5ef93fa1988c7bd6
Filipe Magalhães Moreira <filipe.mm.1999@hotmail.com>
2023-06-20 18:56:02 -0300

    Criação dos Controllers
    Depois dos testes aplicamos as funcionalidades de CRUD em MedicamentoController e fizemos o Login em AdministradorController

---
+++ Controllers/AdministradorController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoBulaFinal.DTOs;
+using ProjetoBulaFinal.Models;
+using ProjetoBulaFinal.Repositorio.Interfaces;
+using ProjetoBulaFinal.Servicos.Autenticacao;
+using ProjetoBulaFinal.Servicos;
+
+namespace ProjetoBulaFinal.Controllers
+{
+    public class AdministradorController : ControllerBase
+    {
+        private IServicoAdm<Administrador> _servico;
+        public AdministradorController(IServicoAdm<Administrador> servico)
+        {
+            _servico = servico;
+        }
+
+        // POST: Administrador
+
+        [HttpPost("/login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] AdministradorDTO administradorDTO)
+        {
+            if (string.IsNullOrEmpty(administradorDTO.Email) || string.IsNullOrEmpty(administradorDTO.Senha))
+                return StatusCode(400, new
+                {
+                    Mensagem = "Preencha o email e a senha"
+                });
+
+            var administrador = await _servico.Login(administradorDTO.Email, administradorDTO.Senha);
+            if (administrador is null)
+                return StatusCode(404, new
+                {
+                    Mensagem = "Usuario ou senha não encontrado em nossa base de dados"
+                });
+
+            var administradorLogado = BuilderServico<AdministradorLogado>.Builder(administrador);
+            administradorLogado.Token = TokensJWT.Builder(administradorLogado);
+
+            return StatusCode(200, administradorLogado);
+        }
+    }
+}
---
+++ Controllers/MedicamentoController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoBulaFinal.DTOs;
+using ProjetoBulaFinal.Models;
+using ProjetoBulaFinal.Repositorio.Interfaces;
+using ProjetoBulaFinal.Servicos;
+
+namespace ProjetoBulaFinal.Controllers
+{
+    [Route("medicamentos")]
+    [ApiController]
+    public class MedicamentoController : ControllerBase
+    {
+        private IServico<Medicamento> _servico;
+        public MedicamentoController(IServico<Medicamento> servico)
+        {
+            _servico = servico;
+        }
+
+        // GET: Medicamentos
+
+        [HttpGet("")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var medicamento = await _servico.TodosAsync();
+            return StatusCode(200, medicamento);
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Details([FromRoute] int id)
+        {
+            var medicamento = (await _servico.TodosAsync()).Find(c => c.Id == id);
+
+            return StatusCode(200, medicamento);
+        }
+
+        // POST: Medicamentos
+        [HttpPost("")]
+        [Authorize(Roles = "anvisa,laboratorio")]
+        public async Task<IActionResult> Create([FromBody] MedicamentoDTO medicamentoDTO)
+        {
+            var medicamento = BuilderServico<Medicamento>.Builder(medicamentoDTO);
+            await _servico.IncluirAsync(medicamento);
+            return StatusCode(201, medicamento);
+        }
+
+
+        // PUT: Medicamentos/"id"
+        [HttpPut("{id}")]
+        [Authorize(Roles = "anvisa,laboratorio")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] Medicamento medicamento)
+        {
+            if (id != medicamento.Id)
+            {
+                return StatusCode(400, new
+                {
+                    Mensagem = "O Id do medicamento precisa bater com o id da URL"
+                });
+            }
+
+            var medicamentoDb = await _servico.AtualizarAsync(medicamento);
+
+            return StatusCode(200, medicamentoDb);
+        }
+
+        // DELETE: Medicamentos/"id"
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "anvisa")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var medicamentoDb = (await _servico.TodosAsync()).Find(c => c.Id == id);
+            if (medicamentoDb is null)
+            {
+                return StatusCode(404, new
+                {
+                    Mensagem = "O medicamento informado não existe"
+                });
+            }
+
+            await _servico.ApagarAsync(medicamentoDb);
+
+            return StatusCode(204);
+        }
+    }
+}
